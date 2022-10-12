5f235d7871446d7affefd9460c106acd0603a79a
unknown <MarcusLaska@protonmail.com>
2022-10-12 17:02:56 +0200

    max items count

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,19 +8,24 @@ function Home( { accountConnected} ) {
 
   const [ ownedTokens, setOwnedTokens ] = useState([]);
   const [ totalCount, setTotalCount ] = useState(0);
+  const [ loaded, setLoaded ] = useState(false);
   
   useEffect(() => {
     if (accountConnected){
       async function fetchData(){
+        setLoaded(false);
         let pageKey = "  ";
         let tokens = [];
         while (pageKey){
           let r = await alchemyClient.nft.getNftsForOwner(accountConnected, {pageKey:pageKey, contractAddresses:[process.env.REACT_APP_ERC721_TOKEN_ADDRESS]});
           pageKey = r.pageKey;
           tokens.push(...r.ownedNfts);
-          setTotalCount(r.totalCount);
+          setTotalCount(tokens.length);
+          if (tokens.length >= process.env.REACT_APP_GALLERY_MAX_ITEMS_COUNT)
+          break;
         }
         setOwnedTokens(tokens);
+        setLoaded(true);
       }
       fetchData();
     }
@@ -34,7 +39,7 @@ function Home( { accountConnected} ) {
         <div className="col-md-12">
           <div className="card">
             <div className="card-body">
-              <ItemList tokens={ownedTokens} totalCount={totalCount} accountConnected={accountConnected} />
+              <ItemList tokens={ownedTokens} totalCount={totalCount} accountConnected={accountConnected} loaded={loaded}/>
             </div>
           </div>
         </div>
